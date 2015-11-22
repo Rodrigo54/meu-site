591de1eab48db35b4a8227fe1d1894f3c6dfe4c4
Rodrigo Alves <rodrigo54mix@gmail.com>
2015-11-22 10:57:34 -0300

    update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,7 +19,7 @@ module.exports = function(grunt) {
         dest: './',
         exclusions: [
           '**/.DS_Store',
-          '**/Thumbs.db', 
+          '**/Thumbs.db',
           '.git',
           'README.md',
           'node_modules',
@@ -32,10 +32,36 @@ module.exports = function(grunt) {
           '.gitignore',
           '.grunt'
         ],
-        /*keep: [
-          'application/cache/*',
-          'application/logs/*',
-        ]*/
+        keep: [
+          'controle_lab'
+        ]
+      },
+      build2: {
+        auth: {
+          host: 'ftp.rodrigom.tk',
+          port: 21,
+          authKey: 'key2'
+        },
+        src: './',
+        dest: './',
+        exclusions: [
+          '**/.DS_Store',
+          '**/Thumbs.db',
+          '.git',
+          'README.md',
+          'node_modules',
+          'grunt',
+          'grunt.cmd',
+          'package.json',
+          'Gruntfile.js',
+          '.ftppass',
+          '.gitattributes',
+          '.gitignore',
+          '.grunt'
+        ],
+        keep: [
+          'controle_lab'
+        ]
       }
     },
   });
@@ -44,6 +70,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-ftpush');
 
   // Default task(s).
-  grunt.registerTask('ftp','ftpush');
+  grunt.registerTask('ftp1','ftpush:build');
+  grunt.registerTask('ftp2','ftpush:build2');
 
 };
