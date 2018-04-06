8113f977bbea73c70d9e4c3b73a1725b12d72c1d
Rodrigo Alves <rodrigo54mix@gmail.com>
2018-04-06 20:55:03 -0300

    fix erro load not a function

--- src/main.js
+++ src/main.js
@@ -37,12 +37,14 @@ function fitVideo() {
 
 };
 
-$(window).load(function(){
-  $(".texto").typed({
-    strings: ["WebMaster", "Designer", "Geek","Rodrigo"],
-    typeSpeed: 100,
-    startDelay: 10,
-    backSpeed: 100,
-    backDelay: 500,
+$(window).ready(function(){
+  $(window).load(function(){
+    $(".texto").typed({
+      strings: ["WebMaster", "Designer", "Geek","Rodrigo"],
+      typeSpeed: 100,
+      startDelay: 10,
+      backSpeed: 100,
+      backDelay: 500,
+    });
   });
 });
